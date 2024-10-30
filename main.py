c44f262bf69e168ebc936d937eeede3361fa21d3
ZhouYuqian <zhouyuqian@buaa.edu.cn>
2024-10-30 17:44:32 +0800

    Format the output

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import time
 
 class File:
 
-    def __init__(self, timestamp: float, filepath: str):
+    def __init__(self, timestamp: float, filetype: str, filepath: str):
         self.timestamp = timestamp
         self.strftime = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(timestamp))
+        self.filetype = filetype
         self.filepath = os.path.abspath(filepath)
 
     def trashpath(self, rootpath):
@@ -42,22 +43,31 @@ class Trash:
                 if lines[0].strip() == '[Trash Info]':
                     for line in lines[1:]:
                         fields = line.split()
-                        self.files.append(File(float(fields[0]), fields[1]))
+                        self.files.append(File(float(fields[0]), fields[1], fields[2]))
 
     def store(self):
         with open(self.infopath, 'w') as infofile:
             infofile.write('[Trash Info]\n')
             for file in self.files:
-                infofile.write('%s %s\n' % (str(file.timestamp), file.filepath))
+                infofile.write('%s %s %s\n' % (str(file.timestamp), file.filetype, file.filepath))
 
     def list(self):
         if len(self.files) == 0:
             print('[Trash] Void')
         else:
-            print('[Trash] Total %d' % len(self.files))
-            print('\tDeletionTime\t\tFilePath')
+            print('\033[1m[Trash] Total %d\033[0m' % len(self.files))
+            print('\033[1m\tDeletionTime\t\tOriginalPath\033[0m')
             for index, file in enumerate(self.files):
-                print('[%d] \t%s\t%s' % (index, file.strftime, file.filepath))
+                if file.filetype[0] == '*':
+                    if file.filetype[1:] == 'dir':
+                        print('\033[1m[%d]\033[0m \t%s\t\033[0;36m%s*\033[0m' % (index, file.strftime, file.filepath))
+                    else:
+                        print('\033[1m[%d]\033[0m \t%s\t%s*' % (index, file.strftime, file.filepath))
+                else:
+                    if file.filetype == 'dir':
+                        print('\033[1m[%d]\033[0m \t%s\t\033[0;36m%s\033[0m' % (index, file.strftime, file.filepath))
+                    else:
+                        print('\033[1m[%d]\033[0m \t%s\t%s' % (index, file.strftime, file.filepath))
 
     def include(self, file: File):
         ret = os.system('mv %s %s' % (file.filepath, file.trashpath(self.rootpath)))
@@ -69,7 +79,7 @@ class Trash:
         if ret == 0:
             self.files.remove(file)
 
-    def remove(self, file: File):
+    def exclude(self, file: File):
         ret = os.system('rm -r %s' % file.trashpath(self.rootpath))
         if ret == 0:
             self.files.remove(file)
@@ -80,17 +90,42 @@ if __name__ == '__main__':
     with Trash(rootpath) as trash:
         if len(sys.argv) < 2:
             trash.list()
+
         elif sys.argv[1] == 'list':
             trash.list()
+
         elif sys.argv[1] == 'include':
+            files = []
             for filepath in sys.argv[2:]:
-                file = File(time.time(), filepath)
-                trash.include(file)
+                if os.path.isdir(filepath):
+                    filetype = 'dir'
+                else:
+                    filetype = 'file'
+                if os.path.islink(filepath):
+                    filetype = '*' + filetype
+                files.append(File(time.time(), filetype, filepath))
+            for file in files:
+                if os.path.exists(file.filepath):
+                    trash.include(file)
+                    print('\033[1;32m[Trash Include]\033[0;32m %s\033[0m' % file.filepath)
+                else:
+                    print('\033[1;33m[Trash Include]\033[0;33m %s not exists\033[0m' % file.filepath)
+
         elif sys.argv[1] == 'restore':
+            files = []
             for index in sys.argv[2:]:
-                file = trash.files[int(index)]
-                trash.restore(file)
-        elif sys.argv[1] == 'remove':
+                files.append(trash.files[int(index)])
+            for file in files:
+                if os.path.exists(file.filepath):
+                    print('\033[1;33m[Trash Restore]\033[0;33m %s already exists\033[0m' % file.filepath)
+                else:
+                    trash.restore(file)
+                    print('\033[1;32m[Trash Restore]\033[0;32m %s\033[0m' % file.filepath)
+
+        elif sys.argv[1] == 'exclude':
+            files = []
             for index in sys.argv[2:]:
-                file = trash.files[int(index)]
-                trash.remove(file)
+                files.append(trash.files[int(index)])
+            for file in files:
+                trash.exclude(file)
+                print('\033[1;31m[Trash Exclude]\033[0;31m %s\033[0m' % file.filepath)
